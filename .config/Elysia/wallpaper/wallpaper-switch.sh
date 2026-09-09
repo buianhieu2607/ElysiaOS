99f52a9fae0fa0f9df657647b6d5b8916a7c61a5
buianhieu2607 <87932007+buianhieu2607@users.noreply.github.com>
2026-09-09 11:12:52 +0700

    Update wallpaper-switch.sh to use binary package awww

--- .config/Elysia/wallpaper/wallpaper-switch.sh
+++ .config/Elysia/wallpaper/wallpaper-switch.sh
@@ -2,12 +2,12 @@
 
 
 if [[ -f ~/.config/hypr/Dark.txt ]]; then
-	swww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Dark -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
+	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Dark -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
 
 elif [[ -f ~/.config/hypr/Light.txt ]]; then
-	swww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
+	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
 
 else
-	swww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
+	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
 	
 fi
