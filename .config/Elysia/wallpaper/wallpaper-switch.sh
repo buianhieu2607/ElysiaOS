2c3bc25b40f128db77708c7c075566d53b9ef6e1
buianhieu2607 <87932007+buianhieu2607@users.noreply.github.com>
2026-09-14 21:23:45 +0700

    Fix my own git commit mistakes in wallpaper-switch.sh

--- .config/Elysia/wallpaper/wallpaper-switch.sh
+++ .config/Elysia/wallpaper/wallpaper-switch.sh
@@ -2,15 +2,6 @@
 
 
 if [[ -f ~/.config/hypr/Dark.txt ]]; then
-<<<<<<< HEAD
-	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/.config/Elysia/wallpaper/Dark -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
-
-elif [[ -f ~/.config/hypr/Light.txt ]]; then
-	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/.config/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
-
-else
-	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/.config/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
-=======
 	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/.config/Elysia/wallpaper/Dark -maxdepth 2 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
 
 elif [[ -f ~/.config/hypr/Light.txt ]]; then
@@ -18,6 +9,5 @@ elif [[ -f ~/.config/hypr/Light.txt ]]; then
 
 else
 	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/.config/Elysia/wallpaper/Light -maxdepth 2 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
->>>>>>> 1e1902d (Fix wallpaper change syntax to use binary awww. Add logic for light/dark theme change)
 	
 fi
