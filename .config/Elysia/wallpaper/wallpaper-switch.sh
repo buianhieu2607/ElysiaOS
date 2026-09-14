6e2b14f870eff1b3a853eae1e611475539165722
buianhieu2607 <87932007+buianhieu2607@users.noreply.github.com>
2026-09-14 17:03:08 +0700

    Fix a directory typo in wallpaper-switch.sh

--- .config/Elysia/wallpaper/wallpaper-switch.sh
+++ .config/Elysia/wallpaper/wallpaper-switch.sh
@@ -2,12 +2,12 @@
 
 
 if [[ -f ~/.config/hypr/Dark.txt ]]; then
-	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Dark -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
+	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/.config/Elysia/wallpaper/Dark -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
 
 elif [[ -f ~/.config/hypr/Light.txt ]]; then
-	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
+	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/.config/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
 
 else
-	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
+	awww img --transition-type grow --transition-step 10 --transition-fps 60 "$(find ~/.config/Elysia/wallpaper/Light -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)"
 	
 fi
